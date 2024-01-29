3eb6ed61e9348fed91d4ab918d73969096cc0083
binisha <sherchanbinisha5@gmail.com>
2024-01-29 16:43:15 +0545

    Update front end

---
+++ include/navbar.php
@@ -0,0 +1,79 @@
+    <!-- ======= Header ======= -->
+    <header id="header" class="">
+    <div class="topbar-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-sm-12">
+                        <div class="topbar-menu">
+                            <ul class="top-contact text-left float-start p-0">
+                                <li>Open Hours : <span class=""> Sun - Fri 10:00-5:00</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-8 col-sm-12">
+                        <div class="topbar-right text-end">
+                            <ul class="top-contact">
+                                <li>
+                                  <i class="bi bi-phone"></i> <a href="tel:061-544440"> 061-544440</a>
+                                </li>
+                                <li>
+                                  <i class="bi bi-envelope"></i> <a href=""> Info@meditouch.com.np</a>
+                                </li>
+                                <li>
+                                  <i class="bi bi-phone"></i> <a href="tel:061-544440"> 061-544440</a>
+                                </li>
+                            
+                          </ul>
+                            <!-- <div class=" list-inline">
+                                <ul class="social-icons">
+                                    <li><a href="https://www.facebook.com/rkdholdings" target="_blank"><i class="bi bi-facebook me-2"></i></a>  </li>
+                                    <li><a href="https://www.youtube.com/channel/UCxh0z_rSb870U6gZ235uOmQ" target="_blank"><i class="bi bi-youtube"></i></a> </li>
+                                </ul>
+                            </div> -->
+                        </div>
+                   </div>
+               </div>
+            </div>
+        </div>
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between nav-head">
+
+      
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <a href="/" 
+         class="logo me-auto me-lg-0">
+         <img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="about.php">Who we are</a></li>
+          <li><a class="nav-link scrollto" href="#menu">What we do</a></li>
+          <li><a class="nav-link scrollto" href="#specials">Packages</a></li>
+          <li><a class="nav-link scrollto" href="#events">Events</a></li>
+         
+          <li><a class="nav-link scrollto" href="#gallery">Gallery</a></li>
+          <li class="dropdown"><a href="#"><span>Events</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+      <!-- <a href="contact.php" class="book-a-table-btn scrollto d-none d-lg-flex">Book us</a> -->
+
+    </div>
+  </header><!-- End Header -->
