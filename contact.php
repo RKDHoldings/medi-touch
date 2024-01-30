b15eb0bc4105826a5450aacad1a3f41b9646e5a5
Shiva Gurung <shivakgrg@gmail.com>
2024-01-30 14:46:29 +0545

    contact us page added

--- contact.php
+++ contact.php
@@ -1,57 +1,118 @@
 <?php
- include('include/html-header.php');
- ?>
+include('include/html-header.php');
+?>
 
 <?php
- include('include/navbar.php');
- ?>
+include('include/navbar.php');
+?>
 
-  <main id="main">
+<main id="main">
 
-    <!-- ======= Breadcrumbs ======= -->
-    <section id="breadcrumbs" class="breadcrumbs">
-      <div class="container">
+  <!-- ======= Breadcrumbs ======= -->
+  <section id="breadcrumbs" class="breadcrumbs">
+    <div class="container">
 
-        <div class="d-flex justify-content-between align-items-center">
-          <h2>Contact</h2>
-          <ol>
-            <li><a href="index.html">Home</a></li>
-            <li>Contact</li>
-          </ol>
-        </div>
-
+      <div class="d-flex justify-content-between align-items-center">
+        <h2>Contact</h2>
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>Contact</li>
+        </ol>
       </div>
-    </section><!-- End Breadcrumbs -->
 
-    <!-- ======= Contact Section ======= -->
-    <div class="map-section">
-       <iframe style="border:0; width: 100%; height: 350px;"
-       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3515.4485912866367!2d83.98870467472418!3d28.224060975890254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39959448c49b01d9%3A0x8b50d30a3249775!2sNova%20Computer%20Institute!5e0!3m2!1sen!2snp!4v1688920263197!5m2!1sen!2snp"   allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
+  </section><!-- End Breadcrumbs -->
 
-    <section id="contact" class="contact">
-      <div class="container">
 
-        <div class="row justify-content-center" data-aos="fade-up">
 
-          <div class="col-lg-10">
+  <!-- ======= Contact Section ======= -->
+  <div class="map-section">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14063.493851078929!2d83.97544390560462!3d28.211156814356034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399595d66423603b%3A0x3da6e17cef7ecb07!2sMediTouch%20Health%20Clinic!5e0!3m2!1sen!2snp!4v1706605013467!5m2!1sen!2snp" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
 
-            <div class="info-wrap">
-              <div class="row">
-                <div class="col-lg-4 info">
-                  <i class="bi bi-geo-alt"></i>
+
+
+  <section id="contact" class="contact">
+
+    <div class="container">
+      <div class="row mt-5 justify-content-center" data-aos="fade-up">
+        <h3 class="mb-5 text-center">Contact Us</h3>
+        <div class="col">
+          <div class="row">
+            <div class="col-12 col-xl-6">
+              <img src="assets/img/model-contact-section.jpg" class="rounded-1 contact-us-img" alt="A girl posing for picture for skin clinic" />
+            </div>
+            <div class="col-12 col-xl-6">
+              <form action="forms/contact.php" method="post" role="form" class="php-email-form border-0">
+                <div class="form-group">
+                  <label for="name" class="form-label fw-bold">Name</label>
+                  <input type="text" name="name" class="form-control custom-border" id="name" placeholder="Your Name" required>
+                </div>
+                <div class=" form-group mt-3">
+                  <label for="email" class="form-label fw-bold">Email</label>
+                  <input type="email" class="form-control custom-border" name="email" id="email" placeholder="Your Email" required>
+                </div>
+                <div class="form-group mt-3">
+                  <label for="subject" class="form-label fw-bold">Subject</label>
+                  <input type="text" class="form-control custom-border" name="subject" id="subject" placeholder="Subject" required>
+                </div>
+                <div class="form-group mt-3">
+                  <label for="message" class="form-label fw-bold">Message</label>
+                  <textarea class="form-control custom-border" name="message" rows="5" placeholder="Message" required></textarea>
+                </div>
+                <div class="my-3">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div>
+                </div>
+                <div class="text-center"><button type="submit">Send Message</button></div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+      <!-- Contact Info Section -->
+      <div class="row justify-content-center" data-aos="fade-up">
+        <div class="col">
+          <div class="info-wrap">
+            <div class="row gx-3">
+              <div class="col-lg-3 ">
+                <div class="info">
+                  <div class="icon-container">
+                    <i class="bi bi-geo-alt-fill"></i>
+                  </div>
                   <h4>Location:</h4>
                   <p>Mahendrapul<br>Pokhara, Kaski</p>
                 </div>
-
-                <div class="col-lg-4 info mt-4 mt-lg-0">
-                  <i class="bi bi-envelope"></i>
+              </div>
+              <div class="col-lg-3">
+                <div class="info">
+                  <div class="icon-container">
+                    <i class="bi bi-envelope-fill"></i>
+                  </div>
                   <h4>Email:</h4>
-                  <p>novainstitute.lp@gmail.com</p>
+                  <p class="mb-4">novainstitute.lp@gmail.com</p>
                 </div>
-
-                <div class="col-lg-4 info mt-4 mt-lg-0">
-                  <i class="bi bi-phone"></i>
+              </div>
+              <div class="col-lg-3">
+                <div class="info">
+                  <div class="icon-container">
+                    <i class="bi bi-telephone-fill"></i>
+                  </div>
+                  <h4>Call:</h4>
+                  <p> 061-544199
+                    <br>061-552741
+                  </p>
+                </div>
+              </div>
+              <div class="col-lg-3">
+                <div class="info">
+                  <div class="icon-container">
+                    <i class="bi bi-clock-fill"></i>
+                  </div>
                   <h4>Call:</h4>
                   <p> 061-544199
                     <br>061-552741
@@ -59,44 +120,17 @@
                 </div>
               </div>
             </div>
-
           </div>
-
         </div>
-
-        <div class="row mt-5 justify-content-center" data-aos="fade-up">
-          <div class="col-lg-10">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-                </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-                </div>
-              </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-              </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
-          </div>
-
-        </div>
-
       </div>
-    </section><!-- End Contact Section -->
+      <!-- End of Contact Info Section  -->
 
-  </main><!-- End #main -->
+    </div>
+  </section>
+  <!-- End Contact Section -->
 
-  <?php
- include('include/footer.php');
- ?>
+</main><!-- End #main -->
+
+<?php
+include('include/footer.php');
+?>
