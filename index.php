bd89892991a7e247266fb31020dd74786bc85011
Shiva Gurung <shivakgrg@gmail.com>
2024-01-31 17:00:16 +0545

    Before after section WIP

--- index.php
+++ index.php
@@ -1,199 +1,232 @@
 <?php
- include('include/html-header.php');
- ?>
- <?php
- include('include/navbar.php');
- ?>
+include('include/html-header.php');
+?>
+<?php
+include('include/navbar.php');
+?>
 
-  <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center">
-    <div class="container position-relative text-center text-lg-start" data-aos="zoom-in" data-aos-delay="100">
-      <div class="row align-items-center">
-        <div class="col-lg-6">
-          <h1>We Are Ready <span>For Your Skin</span>
-Care Help! </h1>
-         
-          <p>Follow us to improve health, <br/>
-to enjoy and relax</p>
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
+  <div class="container position-relative text-center text-lg-start" data-aos="zoom-in" data-aos-delay="100">
+    <div class="row align-items-center">
+      <div class="col-lg-6">
+        <h1>We Are Ready <span>For Your Skin</span>
+          Care Help! </h1>
 
-          <div class="btns">
-            <a href="/" class="btn btn-primary animated fadeInUp scrollto">Contact now
-               <i class="bi bi-chevron-double-right"></i>
-            </a>
-          </div>
+        <p>Follow us to improve health, <br />
+          to enjoy and relax</p>
+
+        <div class="btns">
+          <a href="/" class="btn btn-primary animated fadeInUp scrollto">Contact now
+            <i class="bi bi-chevron-double-right"></i>
+          </a>
         </div>
-        <div class="col-lg-6"  data-aos="zoom-in" data-aos-delay="200">
-            <img src="assets/img/hero.png" alt="">
-            <!-- <a href="https://www.youtube.com/watch?v=u6BOC7CDUTQ" class="glightbox play-btn"></a> -->
-        </div>
-        <!-- <div class="col-lg-4 d-flex align-items-center justify-content-center position-relative" data-aos="zoom-in" data-aos-delay="200">
+      </div>
+      <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="200">
+        <img src="assets/img/hero.png" alt="">
+        <!-- <a href="https://www.youtube.com/watch?v=u6BOC7CDUTQ" class="glightbox play-btn"></a> -->
+      </div>
+      <!-- <div class="col-lg-4 d-flex align-items-center justify-content-center position-relative" data-aos="zoom-in" data-aos-delay="200">
         
         </div> -->
 
+    </div>
+  </div>
+
+
+  <img src="assets/img/polygon.png" class="polygon-icon" alt="">
+
+  <a href="#why-us">
+    <div class="mouse-icon">
+      <div class="wheel"></div>
+    </div>
+  </a>
+
+</section><!-- End Hero -->
+
+<main id="main">
+
+  <section class="about-wrapper">
+    <div class="container" data-aos="fade-up">
+      <div class="row align-items-center">
+        <div class="col-lg-7  d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
+          <div class="image-stack text-end">
+            <img src="assets/img/about-1.png" alt="" class="stack-front">
+            <img src="assets/img/about-2.png" alt="" class="stack-front">
+            <img src="assets/img/about-3.png" alt="" class="stack-back">
+          </div>
+
+        </div>
+        <div class="col-xl-5 col-lg-6 col-md-12 col-sm-12 col-12">
+          <div class="about-info">
+            <div class="heading-wrapper">
+              <h2>
+                We have 7+ Years , <br />Experience.
+              </h2>
+            </div>
+            <p class="mb-3">
+              Follow us to improve health, <br /> to enjoy and relax
+            </p>
+            <div class="more-about">
+              <h4>Our mission and Vision</h4>
+              <p>Follow us to improve health, to enjoy and relax. Follow us to improve health.</p>
+              <a href="" class="btn btn-primary">
+                Who we are
+                <i class="bi bi-chevron-double-right"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+  </section>
+
+
+
+  <!-- services section start  -->
+  <section id="what-we-do">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <h3 class="text-center">What we provide you</h3>
+          <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque.
+            Suspendisse mattis, neque sed blandit auctor, dolor leo efficitur magna, id luctus erat massa nec ipsum.
+            Donec libero justo,</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="d-flex video-wrapper w-100">
+          <div class="video-background">
+            <a href="https://www.youtube.com/watch?v=u6BOC7CDUTQ" class="glightbox play-btn"></a>
+          </div>
+          <div class="video-content">
+            <h3 class="text-start">What we makes different <br /> from others</h3>
+            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque.
+              Suspendisse mattis, neque sed blandit auctor, dolor leo efficitur magna, id luctus erat massa nec ipsum. Donec libero justo,</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque.
+            </p>
+
+            <a href="" class="btn btn-secondary mt-4">
+              View More
+            </a>
+          </div>
+        </div>
+
+
+
+
       </div>
     </div>
-    
-       
-          <img src="assets/img/polygon.png" class="polygon-icon" alt="">
-      
-       <a href="#why-us">
-         <div class="mouse-icon">
-            <div class="wheel"></div>
-         </div>
-       </a>
+  </section>
 
-  </section><!-- End Hero -->
+  <section id="main-services">
+    <div class="container">
+      <div class="row">
+        <h3>What services we provide you</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque.
+          Suspendisse mattis, neque sed blandit auctor, dolor leo efficitur magna, id luctus erat massa nec ipsum. Donec libero justo,</p>
 
-  <main id="main">
+        <div class="col-lg-6">
+          <img src="assets/img/services/1.png" alt="">
+        </div>
+        <div class="col-lg-6">
+          <div class="service-content">
+            <h5>Facial care treatment</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque. Suspendisse mattis, neque sed blandit aucto</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque.</p>
+          </div>
 
-    <section class="about-wrapper">
-            <div class="container" data-aos="fade-up">
-                <div class="row align-items-center">
-                <div class="col-lg-7  d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
-                      <div class="image-stack text-end">
-                        <img src="assets/img/about-1.png" alt="" class="stack-front">
-                        <img src="assets/img/about-2.png" alt="" class="stack-front">
-                        <img src="assets/img/about-3.png" alt="" class="stack-back">
-                      </div>
-                    
-                </div>
-                <div class="col-xl-5 col-lg-6 col-md-12 col-sm-12 col-12">
-                        <div class="about-info">
-                            <div class="heading-wrapper">
-                                <h2>
-                                We have 7+ Years , <br/>Experience.
-                                </h2>
-                            </div>
-                            <p class="mb-3">
-                            Follow us to improve health, <br/>  to enjoy and relax
-                            </p>
-                             <div class="more-about">
-                                  <h4>Our mission and Vision</h4>
-                                  <p>Follow us to improve health, to enjoy and relax. Follow us to improve health.</p>
-                                  <a href="" class="btn btn-primary">
-                                      Who we are 
-                                      <i class="bi bi-chevron-double-right"></i>
-                                  </a>
-                             </div>
-                        </div>
-                </div>
-            </div>
-    </section>
+        </div>
+        <div class="col-lg-6">
+          <div class="service-content">
+            <h5>Facial care treatment</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque. Suspendisse mattis, neque sed blandit aucto</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque.</p>
+          </div>
 
-    
+        </div>
+        <div class="col-lg-6">
+          <img src="assets/img/services/1.png" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
 
-    <!-- services section start  -->
-     <section id="what-we-do">
-         <div class="container">
-             <div class="row">
-                  <div class="col-lg-12">  
-                       <h3 class="text-center">What we provide you</h3>
-                       <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque.
-                         Suspendisse mattis, neque sed blandit auctor, dolor leo efficitur magna, id luctus erat massa nec ipsum.
-                          Donec libero justo,</p>
-                  </div>
-             </div>
 
-             <div class="row">
-                  <div class="d-flex video-wrapper w-100">
-                       <div class="video-background">                           
-                            <a href="https://www.youtube.com/watch?v=u6BOC7CDUTQ" class="glightbox play-btn"></a>
-                       </div>
-                       <div class="video-content">
-                            <h3 class="text-start">What we makes different <br/> from others</h3>
-                            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque.
-                              Suspendisse mattis, neque sed blandit auctor, dolor leo efficitur magna, id luctus erat massa nec ipsum. Donec libero justo,</p>
-                            <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque. 
-                            </p>  
-                            
-                            <a href="" class="btn btn-secondary mt-4">
-                            View More
-                            </a>
-                       </div>
-                  </div>
-                 
-                      
-                 
-                  
-             </div>
-         </div>
-     </section>
 
-     <section id="main-services">
-         <div class="container">
-              <div class="row">
-                 <h3>What services we provide you</h3>
-                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque.
-                   Suspendisse mattis, neque sed blandit auctor, dolor leo efficitur magna, id luctus erat massa nec ipsum. Donec libero justo,</p>
 
-                   <div class="col-lg-6">
-                        <img src="assets/img/services/1.png" alt="">
-                   </div>
-                   <div class="col-lg-6">
-                        <div class="service-content">
-                            <h5>Facial care treatment</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque. Suspendisse mattis, neque sed blandit aucto</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque.</p>   
-                        </div>
-                      
-                   </div>
-                   <div class="col-lg-6">
-                        <div class="service-content">
-                            <h5>Facial care treatment</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque. Suspendisse mattis, neque sed blandit aucto</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque.</p>   
-                        </div>
-                      
-                   </div>
-                   <div class="col-lg-6">
-                        <img src="assets/img/services/1.png" alt="">
-                   </div>
+  <!-- Before and After section Start  -->
+  <section class="before-after mt-5" id="before-after">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <h3 class="text-center">Success Stories</h3>
+          <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque.
+            Suspendisse mattis, neque sed blandit auctor, dolor leo efficitur magna.</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="slider-wrapper">
+          <div class="images">
+            <div class="img-1"></div>
+            <div class="img-2"></div>
+          </div>
+          <div class="slider">
+            <input type="range" min="0" max="100" value="50">
+          </div>
+        </div>
+      </div>
+  </section>
+
+  <!-- Before and After section end  -->
+
+
+
+  <!-- services section start  -->
+
+
+  <!-- Contact section start -->
+  <section id="" class="contact">
+    <div class="container" data-aos="fade-up">
+      <div class="row" data-aos="fade-up" data-aos-delay="100">
+        <div class="heading-wrapper">
+          <h4>
+            Book us
+          </h4>
+          <h2>
+            Where you want Our Services
+          </h2>
+        </div>
+        <div class="col-lg-12" data-aos="fade-up" data-aos-delay="200">
+          <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+            <div class="row">
+              <div class="col-md-6 form-group">
+                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required="">
               </div>
-         </div>
-     </section>
-    <!-- services section end  -->
-     
-     <!-- Contact section start -->
-     <section id="" class="contact">
-           <div class="container"  data-aos="fade-up">
-                <div class="row" data-aos="fade-up" data-aos-delay="100">
-                     <div class="heading-wrapper">
-                            <h4>
-                                Book us
-                            </h4>
-                            <h2>
-                                Where you want Our Services
-                            </h2>
-                     </div>
-                     <div class="col-lg-12" data-aos="fade-up" data-aos-delay="200">
-                        <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                          <div class="row">
-                            <div class="col-md-6 form-group">
-                              <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required="">
-                            </div>
-                            <div class="col-md-6 form-group mt-3 mt-md-0">
-                              <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required="">
-                            </div>
-                          </div>
-                          <div class="form-group mt-3">
-                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required="">
-                          </div>
-                          <div class="form-group mt-3">
-                            <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
-                          </div>
-                          <div class="my-3">
-                            <div class="loading">Loading</div>
-                            <div class="error-message"></div>
-                            <div class="sent-message">Your message has been sent. Thank you!</div>
-                          </div>
-                          <div class="text-center"><button type="submit">Send Message</button></div>
-                        </form>
-                     </div>
-                </div>
-           </div>
-     </section>
-     <!-- Contact section end-->
+              <div class="col-md-6 form-group mt-3 mt-md-0">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required="">
+              </div>
+            </div>
+            <div class="form-group mt-3">
+              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required="">
+            </div>
+            <div class="form-group mt-3">
+              <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
+            </div>
+            <div class="my-3">
+              <div class="loading">Loading</div>
+              <div class="error-message"></div>
+              <div class="sent-message">Your message has been sent. Thank you!</div>
+            </div>
+            <div class="text-center"><button type="submit">Send Message</button></div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Contact section end-->
 
 
 
@@ -202,10 +235,10 @@ to enjoy and relax</p>
 
 
 
-  </main><!-- End #main -->
+</main><!-- End #main -->
 
 
 
-  <?php
- include('include/footer.php');
- ?>
+<?php
+include('include/footer.php');
+?>
