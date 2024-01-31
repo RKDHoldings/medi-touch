a823cbf8ecf29742aa2e7902a7ac8071b066e915
Shiva Gurung <shivakgrg@gmail.com>
2024-01-31 16:23:05 +0545

    Team Member section

--- about.php
+++ about.php
@@ -1,182 +1,188 @@
 <?php
- include('include/html-header.php');
- ?>
+include('include/html-header.php');
+?>
 
 <?php
- include('include/navbar.php');
- ?>
+include('include/navbar.php');
+?>
 
-  <main id="main">
+<main id="main">
 
-    <!-- ======= Breadcrumbs ======= -->
-    <section id="breadcrumbs" class="breadcrumbs">
-      <div class="container">
+  <!-- ======= Breadcrumbs ======= -->
+  <section id="breadcrumbs" class="breadcrumbs">
+    <div class="container">
 
-        <div class="d-flex justify-content-between align-items-center">
-          <h2>About</h2>
-          <ol>
-            <li><a href="index.html">Home</a></li>
-            <li>About</li>
-          </ol>
-        </div>
-
+      <div class="d-flex justify-content-between align-items-center">
+        <h2>About</h2>
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>About</li>
+        </ol>
       </div>
-    </section><!-- End Breadcrumbs -->
 
-    <section class="about-wrapper">
-            <div class="container" data-aos="fade-up">
-                <div class="row align-items-center">
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                        <div class="about-img" data-aos="zoom-in" data-aos-delay="100">
-                            <img src="assets/img/about.png" alt="">
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                        <div class="about-info">
-                            <div class="heading-wrapper">
-                                <h4>
-                                    Our Story
-                                </h4>
-                                <h2>
-                                    Trusted By 200 + satisfied clients
-                                </h2>
-                            </div>
-                            <p class="mb-3">
-                                Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore eit esdioilore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullaemco laboeeiris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor iesdein reprehendeerit
-                                in voluptate velit esse cillum dolore.
-                            </p>
-                            <ul class="story-list">
-                                <li>
-                                    <span>
-                                        <svg enable-background="new 0 0 512 512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><g><path d="m271.176 121.396c-150.205 7.822-271 132.495-271 284.604v106l37.925-88.29c44.854-89.692 133.847-147.041 233.075-152.314v121.318l240.648-196.714-240.648-196z"></path></g></svg>
-                                    </span> Fresh and Fast food Delivery
-                                </li>
-                                <li>
-                                    <span>
-                                        <svg enable-background="new 0 0 512 512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><g><path d="m271.176 121.396c-150.205 7.822-271 132.495-271 284.604v106l37.925-88.29c44.854-89.692 133.847-147.041 233.075-152.314v121.318l240.648-196.714-240.648-196z"></path></g></svg>
-                                    </span> 24/7 Customer Support
-                                </li>
-                                <li>
-                                    <span>
-                                        <svg enable-background="new 0 0 512 512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><g><path d="m271.176 121.396c-150.205 7.822-271 132.495-271 284.604v106l37.925-88.29c44.854-89.692 133.847-147.041 233.075-152.314v121.318l240.648-196.714-240.648-196z"></path></g></svg>
-                                    </span> Easy Customization Options
-                                </li>
-                                <li>
-                                    <span>
-                                        <svg enable-background="new 0 0 512 512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><g><path d="m271.176 121.396c-150.205 7.822-271 132.495-271 284.604v106l37.925-88.29c44.854-89.692 133.847-147.041 233.075-152.314v121.318l240.648-196.714-240.648-196z"></path></g></svg>
-                                    </span> Delicious Deals for Delicious Meals
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    </section>
+    </div>
+  </section><!-- End Breadcrumbs -->
 
-     <!-- ======= Our Team Section ======= -->
-     <section id="team" class="team section-bg">
-      <div class="container">
-
-        <div class="section-title" data-aos="fade-up">
-          <h2>Our <strong>Team</strong></h2>
-          <p>
-           Our company thrives with the contribution of our team. We are grateful towards everyone for their continued support to achieve our vision.
-          </p>
+  <section class="about-wrapper">
+    <div class="container" data-aos="fade-up">
+      <div class="row align-items-center">
+        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+          <div class="about-img" data-aos="zoom-in" data-aos-delay="100">
+            <img src="assets/img/about.png" alt="">
+          </div>
         </div>
-
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up">
-              <div class="member-img">
-                <img src="assets/img/team/doctorTeam.jpg" class="img-fluid" alt="">
-                <div class="social">
-                  <a href=""><i class="bi bi-facebook"></i></a>
-                  <a href=""><i class="bi bi-instagram"></i></a>
-                </div>
-              </div>
-              <div class="member-info ">
-                <h4 class="">Sabi Sherchan</h4>
-                <span>Managing Director</span>
-              </div>
+        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+          <div class="about-info">
+            <div class="heading-wrapper">
+              <h4>
+                Our Story
+              </h4>
+              <h2>
+                Trusted By 200 + satisfied clients
+              </h2>
             </div>
+            <p class="mb-3">
+              Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore eit esdioilore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullaemco laboeeiris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor iesdein reprehendeerit
+              in voluptate velit esse cillum dolore.
+            </p>
+            <ul class="story-list">
+              <li>
+                <span>
+                  <svg enable-background="new 0 0 512 512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                      <path d="m271.176 121.396c-150.205 7.822-271 132.495-271 284.604v106l37.925-88.29c44.854-89.692 133.847-147.041 233.075-152.314v121.318l240.648-196.714-240.648-196z"></path>
+                    </g>
+                  </svg>
+                </span> Fresh and Fast food Delivery
+              </li>
+              <li>
+                <span>
+                  <svg enable-background="new 0 0 512 512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                      <path d="m271.176 121.396c-150.205 7.822-271 132.495-271 284.604v106l37.925-88.29c44.854-89.692 133.847-147.041 233.075-152.314v121.318l240.648-196.714-240.648-196z"></path>
+                    </g>
+                  </svg>
+                </span> 24/7 Customer Support
+              </li>
+              <li>
+                <span>
+                  <svg enable-background="new 0 0 512 512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                      <path d="m271.176 121.396c-150.205 7.822-271 132.495-271 284.604v106l37.925-88.29c44.854-89.692 133.847-147.041 233.075-152.314v121.318l240.648-196.714-240.648-196z"></path>
+                    </g>
+                  </svg>
+                </span> Easy Customization Options
+              </li>
+              <li>
+                <span>
+                  <svg enable-background="new 0 0 512 512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                      <path d="m271.176 121.396c-150.205 7.822-271 132.495-271 284.604v106l37.925-88.29c44.854-89.692 133.847-147.041 233.075-152.314v121.318l240.648-196.714-240.648-196z"></path>
+                    </g>
+                  </svg>
+                </span> Delicious Deals for Delicious Meals
+              </li>
+            </ul>
           </div>
-
-          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up" data-aos-delay="100">
-              <div class="member-img">
-                <img src="assets/img/team/doctorTeam.jpg" class="img-fluid" alt="">
-                <div class="social">
-                  <a href=""><i class="bi bi-facebook"></i></a>
-                  <a href=""><i class="bi bi-instagram"></i></a>
-                </div>
-              </div>
-              <div class="member-info">
-                <h4>Anju Chhantyal</h4>
-                <span>Japanese Language Instructor</span>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up" data-aos-delay="200">
-              <div class="member-img">
-                <img src="assets/img/team/doctorTeam.jpg" class="img-fluid" alt="">
-                <div class="social">
-                  <a href=""><i class="bi bi-facebook"></i></a>
-                  <a href=""><i class="bi bi-instagram"></i></a>
-                </div>
-              </div>
-              <div class="member-info">
-                <h4>Neelam Thapa</h4>
-                <span>Counsellor</span>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up" data-aos-delay="300">
-              <div class="member-img">
-                <img src="assets/img/team/doctorTeam.jpg" class="img-fluid" alt="">
-                <div class="social">
-                  <a href=""><i class="bi bi-facebook"></i></a>
-                  <a href=""><i class="bi bi-instagram"></i></a>
-                  
-                </div>
-              </div>
-              <div class="member-info">
-                <h4>Isha Thapa</h4>
-                <span>Receptionist</span>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up" data-aos-delay="300">
-              <div class="member-img">
-                <img src="assets/img/team/doctorTeam.jpg" class="img-fluid" alt="">
-                <div class="social">
-                  <a href=""><i class="bi bi-facebook"></i></a>
-                  <a href=""><i class="bi bi-instagram"></i></a>
-                  
-                </div>
-              </div>
-              <div class="member-info">
-                <h4>Kanchan Gurung </h4>
-                <span>Receptionist</span>
-              </div>
-            </div>
-          </div>
-
         </div>
-
       </div>
-    </section><!-- End Our Team Section -->
+    </div>
+  </section>
 
-   
+  <!-- ======= Our Team Section ======= -->
+  <section id="team" class="team section-bg pt-5 mt-5">
+    <div class="container">
 
-  </main><!-- End #main -->
+      <div class="section-title" data-aos="fade-up">
+        <h2>Meet the <strong>Specialists</strong></h2>
+        <p>
+          Our company thrives with the contribution of our team. We are grateful towards everyone for their continued support to achieve our vision.
+        </p>
+      </div>
 
-  <?php
- include('include/footer.php');
- ?>
+      <div class="row">
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+          <div class="member" data-aos="fade-up">
+            <div class="member-img">
+              <img src="assets/img/team/doctorTeam.jpg" class="img-fluid" alt="">
+              <div class="social">
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-instagram"></i></a>
+              </div>
+            </div>
+            <div class="member-info ">
+              <h4 class="">Sabi Sherchan</h4>
+              <span>BSc (Hons), PhD, FRCSs</span>
+              <p>Distinguished dermatologist elevating skincare at renowned clinic.</p>
+              <a href="team.php">Learn More</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+          <div class="member" data-aos="fade-up" data-aos-delay="100">
+            <div class="member-img">
+              <img src="assets/img/team/doctorTeam.jpg" class="img-fluid" alt="">
+              <div class="social">
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-instagram"></i></a>
+              </div>
+            </div>
+            <div class="member-info">
+              <h4>Anju Chhantyal</h4>
+              <span>BSc (Hons), PhD, FRCSs</span>
+              <p>Distinguished dermatologist elevating skincare at renowned clinic.</p>
+              <a href="team-detail.php">Learn More</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+          <div class="member" data-aos="fade-up" data-aos-delay="200">
+            <div class="member-img">
+              <img src="assets/img/team/doctorTeam.jpg" class="img-fluid" alt="">
+              <div class="social">
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-instagram"></i></a>
+              </div>
+            </div>
+            <div class="member-info">
+              <h4>Neelam Thapa</h4>
+              <span>BSc (Hons), PhD, FRCSs</span>
+              <p>Distinguished dermatologist elevating skincare at renowned clinic.</p>
+              <a href="team-detail.php">Learn More</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+          <div class="member" data-aos="fade-up" data-aos-delay="300">
+            <div class="member-img">
+              <img src="assets/img/team/doctorTeam.jpg" class="img-fluid" alt="">
+              <div class="social">
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-instagram"></i></a>
+
+              </div>
+            </div>
+            <div class="member-info">
+              <h4>Isha Thapa</h4>
+              <span>BSc (Hons), PhD, FRCSs</span>
+              <p>Distinguished dermatologist at renowned clinic.</p>
+              <a href="team-detail.php">Learn More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End Our Team Section -->
+
+
+
+</main><!-- End #main -->
+
+<?php
+include('include/footer.php');
+?>
