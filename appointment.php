c56de1af4f17d703e2ede7f370a01e4b2fbcccae
Shiva Gurung <shivakgrg@gmail.com>
2024-02-02 15:10:32 +0545

    Appointment Banner

---
+++ appointment.php
@@ -0,0 +1,51 @@
+<?php
+include('include/html-header.php');
+?>
+
+<?php
+include('include/navbar.php');
+?>
+
+<main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+        <div class="container">
+
+            <div class="d-flex justify-content-between align-items-center">
+                <h2>Book an appointment</h2>
+                <ol>
+                    <li><a href="index.html">Home</a></li>
+                    <li>Book an appointment</li>
+                </ol>
+            </div>
+
+        </div>
+    </section><!-- End Breadcrumbs -->
+
+
+
+    <!-- Book an appointment section Start  -->
+    <section id="appointment" class="appointment">
+        <div class="container p-0">
+            <div class="appointment-banner">
+                <div class="row">
+                    <div class="col-6 me-auto text-content">
+                        <div class="px-4">
+                            <h3 class="title">Book an appointment today</h3>
+                            <p class="subtitle">Transform your look with our expert care</p>
+                            <button class="cta">Book Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Book an appointment section end  -->
+
+</main><!-- End #main -->
+
+<?php
+include('include/footer.php');
+?>
--- index.php
+++ index.php
@@ -154,58 +154,6 @@ include('include/navbar.php');
   </section>
 
 
-
-
-  <!-- Before and After section Start  -->
-  <section class="before-after my-5" id="before-after">
-    <div class="container mb-5">
-      <div class="row mt-5">
-        <div class="col-lg-12">
-          <h3 class="text-center my-4">Success Stories</h3>
-          <div class="row">
-            <p class="text-center col-md-8 mx-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque. Suspendisse mattis, neque sed blandit auctor, dolor leo efficitur magna, id luctus erat massa nec ipsum. Donec libero justo,Suspendisse mattis, neque sed blandit auctor, dolor leo efficitur magna</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="row gx-5 my-5">
-        <div class="col-md-6 mb-5 slider1">
-          <div class="slider-wrapper">
-            <div class="images">
-              <div class="img-1"></div>
-              <div class="img-2"></div>
-            </div>
-            <div class="slider">
-              <div class="drag-line">
-                <span></span>
-              </div>
-              <input type="range" min="0" max="100" value="50">
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 mb-5 slider2">
-          <div class="slider-wrapper">
-            <div class="images">
-              <div class="img-1"></div>
-              <div class="img-2"></div>
-            </div>
-            <div class="slider">
-              <div class="drag-line">
-                <span></span>
-              </div>
-              <input type="range" min="0" max="100" value="50">
-            </div>
-          </div>
-        </div>
-      </div>
-  </section>
-
-
-  <!-- Before and After section end  -->
-
-
-
-
   <!-- services section start  -->
 
 
