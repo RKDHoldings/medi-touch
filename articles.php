01ef0369203d4cc081510ab0ae0423c11a7d715b
Shiva Gurung <shivakgrg@gmail.com>
2024-02-08 16:35:47 +0545

    added articles detail page

--- articles.php
+++ articles.php
@@ -21,54 +21,8 @@ include('include/navbar.php');
       </div>
 
     </div>
-  </section><!-- End Breadcrumbs -->
-
-
-
-  <!-- Before and After section Start  -->
-  <!-- <section class="before-after" id="before-after">
-    <div class="container mb-5">
-      <div class="row mt-5">
-        <div class="col-lg-12">
-          <h3 class="text-center my-4">Success Stories</h3>
-          <div class="row">
-            <p class="text-center col-md-8 mx-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque. Suspendisse mattis, neque sed blandit auctor, dolor leo efficitur magna, id luctus erat massa nec ipsum. Donec libero justo,Suspendisse mattis, neque sed blandit auctor, dolor leo efficitur magna</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="row gx-5 my-5">
-        <div class="col-xl-6 mb-5 slider1">
-          <div class="slider-wrapper">
-            <div class="images">
-              <div class="img-1"></div>
-              <div class="img-2"></div>
-            </div>
-            <div class="slider">
-              <div class="drag-line">
-                <span></span>
-              </div>
-              <input type="range" min="0" max="100" value="50">
-            </div>
-          </div>
-        </div>
-        <div class="col-xl-6 mb-5 slider2">
-          <div class="slider-wrapper">
-            <div class="images">
-              <div class="img-1"></div>
-              <div class="img-2"></div>
-            </div>
-            <div class="slider">
-              <div class="drag-line">
-                <span></span>
-              </div>
-              <input type="range" min="0" max="100" value="50">
-            </div>
-          </div>
-        </div>
-      </div>
-  </section> -->
-  <!-- Before and After section end  -->
+  </section>
+  <!-- End Breadcrumbs -->
 
 
   <!-- News and Blogs Section Start -->
@@ -78,7 +32,7 @@ include('include/navbar.php');
         <div class="col-10 mx-auto">
       <div class="row gx-4 gy-5">
         <div class="col-md-6 col-xl-4">
-          <div class="card border-0 img-date">
+          <div onclick="window.location.href='article-detail.php';" class="card border-0 img-date">
             <div class="img-container">
               <img src="assets/img/article/article.jpg" class="card-img-top rounded-0" alt="A female medical professional working on laptop">
               <div class="date">
@@ -93,7 +47,7 @@ include('include/navbar.php');
           </div>
         </div>
         <div class="col-md-6 col-xl-4">
-          <div class="card border-0">
+          <div onclick="window.location.href='appointment.php';" class="card border-0">
             <div class="img-container">
               <img src="assets/img/article/article.jpg" class="card-img-top rounded-0" alt="A female medical professional working on laptop">
               <div class="date">
@@ -108,7 +62,7 @@ include('include/navbar.php');
           </div>
         </div>
         <div class="col-md-6 col-xl-4">
-          <div class="card border-0">
+          <div onclick="window.location.href='appointment.php';" class="card border-0">
             <div class="img-container">
               <img src="assets/img/article/article.jpg" class="card-img-top rounded-0" alt="A female medical professional working on laptop">
               <div class="date">
@@ -123,7 +77,7 @@ include('include/navbar.php');
           </div>
         </div>
         <div class="col-md-6 col-xl-4">
-          <div class="card border-0 img-date">
+          <div onclick="window.location.href='appointment.php';" class="card border-0 img-date">
             <div class="img-container">
               <img src="assets/img/article/article.jpg" class="card-img-top rounded-0" alt="A female medical professional working on laptop">
               <div class="date">
@@ -138,7 +92,7 @@ include('include/navbar.php');
           </div>
         </div>
         <div class="col-md-6 col-xl-4">
-          <div class="card border-0">
+          <div onclick="window.location.href='appointment.php';" class="card border-0">
             <div class="img-container">
               <img src="assets/img/article/article.jpg" class="card-img-top rounded-0" alt="A female medical professional working on laptop">
               <div class="date">
@@ -153,7 +107,7 @@ include('include/navbar.php');
           </div>
         </div>
         <div class="col-md-6 col-xl-4">
-          <div class="card border-0">
+          <div onclick="window.location.href='appointment.php';" class="card border-0">
             <div class="img-container">
               <img src="assets/img/article/article.jpg" class="card-img-top rounded-0" alt="A female medical professional working on laptop">
               <div class="date">
