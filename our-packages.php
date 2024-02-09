e542ca28d85883d8a82cb84161c0eb1122290d1c
Shiva Gurung <shivakgrg@gmail.com>
2024-02-09 15:45:11 +0545

    package card wip

--- our-packages.php
+++ our-packages.php
@@ -13,10 +13,10 @@ include('include/navbar.php');
     <div class="container">
 
       <div class="d-flex justify-content-between align-items-center">
-        <h2>News & Blogs</h2>
+        <h2>Our Packages</h2>
         <ol>
           <li><a href="index.html">Home</a></li>
-          <li>News & Blogs</li>
+          <li>Our Packages</li>
         </ol>
       </div>
 
@@ -31,6 +31,18 @@ include('include/navbar.php');
       <h3 class="page-title">
         Our Packages
       </h3>
+      <div class="row">
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+          <div class="icon">
+            <img src="assets/img/packages/tulip-aesthetic.svg" height="50" alt="An aesthetic tulip icon"/>
+          </div>
+          <div class="body">
+            <h5 class="title">Aesthetic Procedures</h5>
+            <p class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            <a class="cta">Learn More</a>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
   <!-- News and Blogs Section End -->
