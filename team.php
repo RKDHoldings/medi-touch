191d5bf2967ee81ee862d57e3a7d3b30d59e8d70
Shiva Gurung <shivakgrg@gmail.com>
2024-02-25 13:05:18 +0545

    Added Staff Section in Team Page

---
+++ team.php
@@ -0,0 +1,176 @@
+<?php
+include('include/html-header.php');
+?>
+
+<?php
+include('include/navbar.php');
+?>
+
+<main id="main">
+
+  <!-- ======= Breadcrumbs ======= -->
+  <section id="breadcrumbs" class="breadcrumbs">
+    <div class="container">
+
+      <div class="">
+        <h2>Staffs</h2>
+        <ol>
+          <li><a href="/">Home</a></li>
+          <li>Staffs</li>
+        </ol>
+      </div>
+
+    </div>
+  </section><!-- End Breadcrumbs -->
+
+  <!-- ======= Our Team Section ======= -->
+  <section id="team" class="team section-bg py-5">
+    <div class="container">
+
+      <div class="section-title" data-aos="fade-up">
+        <h2>Meet our <strong>Doctors</strong></h2>
+        <div class="row my-4">
+          <div class="col-7 mx-auto">
+            <p class="text-center ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque.
+              Suspendisse mattis, neque sed blandit auctor, dolor leo efficitur magna, id luctus erat massa nec ipsum.
+              Donec libero justo,</p>
+          </div>
+        </div>
+      </div>
+
+     
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up">
+              <div class="member-img">
+                <img src="https://images.unsplash.com/photo-1612349316228-5942a9b489c2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid" alt="">
+
+              </div>
+              <div class="member-info ">
+                <h4 class="">Gautam</h4>
+                <span>BSc (Hons), PhD, FRCSs</span>
+                <p>Distinguished dermatologist elevating skincare at renowned clinic.</p>
+                <a href="doctors-detail.php">Learn More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div class="member-img">
+                <img src="https://images.unsplash.com/photo-1612349316228-5942a9b489c2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid" alt="">
+
+              </div>
+              <div class="member-info">
+                <h4>Jems Pradhan</h4>
+                <span>BSc (Hons), PhD, FRCSs</span>
+                <p>Distinguished dermatologist elevating skincare at renowned clinic.</p>
+                <a href="doctors-detail.php">Learn More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="200">
+              <div class="member-img">
+                <img src="https://images.unsplash.com/photo-1612349316228-5942a9b489c2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid" alt="">
+
+              </div>
+              <div class="member-info">
+                <h4>John Doe</h4>
+                <span>BSc (Hons), PhD, FRCSs</span>
+                <p>Distinguished dermatologist elevating skincare at renowned clinic.</p>
+                <a href="doctors-detail.php">Learn More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="300">
+              <div class="member-img">
+                <img src="https://images.unsplash.com/photo-1612349316228-5942a9b489c2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid" alt="">
+
+
+              </div>
+              <div class="member-info">
+                <h4>John Doe</h4>
+                <span>BSc (Hons), PhD, FRCSs</span>
+                <p>Distinguished dermatologist at renowned clinic.</p>
+                <a href="doctors-detail.php">Learn More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+     <section class="staff-section mt-4">
+     <div class="section-title mt-5" data-aos="fade-up">
+        <h2>Meet our <strong>Staffs</strong></h2>
+        <div class="row my-4">
+          <div class="col-7 mx-auto">
+            <p class="text-center ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dapibus iaculis scelerisque.
+              Suspendisse mattis, neque sed blandit auctor, dolor leo efficitur magna, id luctus erat massa nec ipsum.
+              Donec libero justo,</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+          <div class="member" data-aos="fade-up" data-aos-delay="300">
+            <div class="member-img">
+              <img src="https://images.unsplash.com/photo-1612349316228-5942a9b489c2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid" alt="">
+            </div>
+            <div class="member-info">
+              <h4>John Doe</h4>
+              <span>Receptionist</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+          <div class="member" data-aos="fade-up" data-aos-delay="300">
+            <div class="member-img">
+              <img src="https://images.unsplash.com/photo-1612349316228-5942a9b489c2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid" alt="">
+            </div>
+            <div class="member-info">
+              <h4>John Doe</h4>
+              <span>Accountant</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+          <div class="member" data-aos="fade-up" data-aos-delay="300">
+            <div class="member-img">
+              <img src="https://images.unsplash.com/photo-1612349316228-5942a9b489c2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid" alt="">
+            </div>
+            <div class="member-info">
+              <h4>John Doe</h4>
+              <span>Counseller</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+          <div class="member" data-aos="fade-up" data-aos-delay="300">
+            <div class="member-img">
+              <img src="https://images.unsplash.com/photo-1612349316228-5942a9b489c2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid" alt="">
+            </div>
+            <div class="member-info">
+              <h4>John Doe</h4>
+              <span>Receptionist</span>
+            </div>
+          </div>
+        </div>
+
+      </div>
+     </section>
+
+    </div>
+  </section><!-- End Our Team Section -->
+
+  <?php
+  include('include/footer.php');
+  ?>
